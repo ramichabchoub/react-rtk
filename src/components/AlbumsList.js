43f97816b7bbf1ce0893a793132d8e9aab94ae8d
Chabchoub Rami <ramichabchoub.rc@gmail.com>
2023-02-11 12:38:26 +0100

    first commit

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,90 @@
+// Method 1:
+
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
+
+function AlbumsList({ user }) {
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      return <AlbumsListItem key={album.id} album={album} />;
+    });
+  }
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
+}
+
+export default AlbumsList;
+
+// Method 2:
+
+// import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+// import Skeleton from "./Skeleton";
+// import Button from "./Button";
+// import AlbumsListItem from "./AlbumsListItem";
+
+// function AlbumsList({ user }) {
+//   const { data, error, isFetching } = useFetchAlbumsQuery(user);
+//   const [addAlbum, results] = useAddAlbumMutation();
+
+//   const handleAddAlbum = () => {
+//     addAlbum(user);
+//   };
+
+//   let errorMessage, content;
+//   if (results.error) {
+//     errorMessage = (
+//       <div className="text-red-500">
+//         Error adding album: {results.error.message}
+//       </div>
+//     );
+//   }
+
+//   if (results.error || data) {
+//     content = data.map((album) => {
+//       return <AlbumsListItem key={album.id} album={album} />;
+//     });
+//   } else if (isFetching) {
+//     content = <Skeleton className="h-10 w-full" times={3} />;
+//   } else if (error) {
+//     content = <div>Error loading albums.</div>;
+//   }
+
+//   return (
+//     <div>
+//       <div className="m-2 flex flex-row items-center justify-between">
+//         <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+//         <Button loading={results.isLoading} onClick={handleAddAlbum}>
+//           + Add Album
+//         </Button>
+//       </div>
+//       {errorMessage}
+//       <div>{content}</div>
+//     </div>
+//   );
+// }
+
+// export default AlbumsList;
